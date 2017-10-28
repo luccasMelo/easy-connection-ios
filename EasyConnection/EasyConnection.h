2e9d2981fe065d273ce0b048e4e5401f4f23d144
Luccas Melo <luccasmelo@luccass-mbp>
2017-10-28 16:39:13 -0200

    Initial Commit

---
+++ EasyConnection/EasyConnection.h
@@ -0,0 +1,19 @@
+//
+//  EasyConnection.h
+//  EasyConnection
+//
+//  Created by Luccas Melo on 28/10/2017.
+//  Copyright © 2017 Luccas Melo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyConnection.
+FOUNDATION_EXPORT double EasyConnectionVersionNumber;
+
+//! Project version string for EasyConnection.
+FOUNDATION_EXPORT const unsigned char EasyConnectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyConnection/PublicHeader.h>
+
+
